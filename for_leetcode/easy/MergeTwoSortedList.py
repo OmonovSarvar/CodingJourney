c42d6a5bf501cf7b69415f77c6d6ce7a47cc3d1a
Sarvar Omonov <sarvaromon33@gmail.com>
2025-09-06 16:27:02 +0500

    Implement iterative merge of two sorted linked lists using dummy node
    - Added Solution.mergeTwoLists to merge two sorted singly-linked lists
    - Used a dummy head node (`total_list`) to simplify list construction
    - Traversed both lists with `current` pointer and appended the smaller node at each step
    - Linked the remainder of whichever list was not exhausted
    - Returned merged list starting from dummy.next

--- for_leetcode/easy/MergeTwoSortedList.py
+++ for_leetcode/easy/MergeTwoSortedList.py
@@ -2,12 +2,30 @@
 # https://leetcode.com/problems/merge-two-sorted-lists/description/
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: # type: ignore
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: 
+        total_list = ListNode()
+        current = total_list
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+        return total_list.next
         # Men yangi ro'yxat yarataman
         # Men ikkala ro'yxatni ham oxiriga yetgunicha ishlayman:
 
---
+++ for_leetcode/hard/MOtMAEZ.py
@@ -0,0 +1,3 @@
+# Minimum Operators to Make Array Elements Zero
+# https://leetcode.com/problems/minimum-operators-to-make-array-elements-zero/description/
+
